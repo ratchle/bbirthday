5c4744aa0400d95a68d43e94dc70bc8618696dde
ratchle <magykaldino@gmail.com>
2025-04-16 22:02:44 -0400

    2nd commmit

---
+++ xyz.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const clic = document.querySelector("#clic");
+    const hi = document.querySelector("#hi");
+    const gif = document.querySelector("#gif");
+    const text = document.querySelector("#text p");
+
+    clic.addEventListener("click", () => {
+        gif.src = "resources/im-lazy-cute.gif"; // second gif
+        text.innerHTML = "hi billy (with one l)";
+        clic.style.display = "none";             // hide first button
+        hi.style.display = "inline-block";       // show second
+    });
+
+    hi.addEventListener("click", () => {
+        gif.src = "resources/peach-goma.gif";    // final gif
+        text.innerHTML = "happy birthday nigga";
+        hi.style.display = "none";               // hide second
+    });
+});
+
